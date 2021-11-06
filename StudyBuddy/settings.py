44333e31671886f781eab8c6913c5f1908e9cda8
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-11-06 23:15:02 +0530

    Added Files

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1f04w)o*ng&=-k8ocj_d1jzqap+e#u(nte1%d2y#gxr#!@#vwi'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'StudyBuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/ 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- base/views.py
+++ base/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Hi")
+rooms=[
+    {'id':1,'name':'Go Python!!'},
+    {'id':2,'name':'Go DSA!!'},
+    {'id':3,'name':'Go Js!!'},
+]
 
-def room(request):
-    return HttpResponse("Room")
+def home(request):
+    return render(request,'base/home.html',{'rooms':rooms})
+
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i
+    context={'room':room}
+    return render(request,'base/room.html',context)
