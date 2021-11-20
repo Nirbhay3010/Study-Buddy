f5f3f1b5566b990d1b2c3807c0cea573f2637d95
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-11-21 01:16:54 +0530

    Added Forms and Database

--- base/views.py
+++ base/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-
-rooms=[
-    {'id':1,'name':'Go Python!!'},
-    {'id':2,'name':'Go DSA!!'},
-    {'id':3,'name':'Go Js!!'},
-]
+from .models import Room
+from .forms import RoomForm
+# rooms=[
+#     {'id':1,'name':'Go Python!!'},
+#     {'id':2,'name':'Go DSA!!'},
+#     {'id':3,'name':'Go Js!!'},
+# ]
 
 def home(request):
-    return render(request,'base/home.html',{'rooms':rooms})
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room = i
+    room=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method =='POST':
+        print(requst.POST)
+    context= {'form':form}
+    return render(request,'base/room_form.html',context)
