afa99fad0566f734f179372edcc2984e4ae1ce16
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-11-21 14:54:27 +0530

    Added Update and Delete Functionallity

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 # Create your views here.
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
 # rooms=[
 #     {'id':1,'name':'Go Python!!'},
@@ -11,7 +11,8 @@ from .forms import RoomForm
 
 def home(request):
     rooms=Room.objects.all()
-    context={'rooms':rooms}
+    topics = Topic.objects.all()
+    context={'rooms':rooms, 'topics': topics}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
@@ -32,5 +33,19 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    if request.method =='POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
     context={'form':form}
     return render(request, 'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method=="POST":
+        room.delete()
+        return redirect('homepage')
+    return render(request,'base/delete.html',{'obj':room})
+
+    
