faa810e46a39dcf66fbf868d0b9399d014dd8001
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-11-21 16:28:51 +0530

    Added Search Bar for rooms

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http.response import HttpResponse
 # Create your views here.
 from .models import Room,Topic
@@ -10,9 +11,15 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms=Room.objects.all()
+    query = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=query) |
+        Q(name__icontains=query) |
+        Q(description__icontains=query)
+        )
     topics = Topic.objects.all()
-    context={'rooms':rooms, 'topics': topics}
+    room_count = rooms.count()
+    context={'rooms':rooms, 'topics': topics, 'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
