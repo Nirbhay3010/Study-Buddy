2c2796edf00584907118f83ec08e689d708b0cf3
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-11-23 22:31:42 +0530

    Added Messages CRUD

--- base/views.py
+++ base/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Room,Topic
+from .models import Message, Room,Topic,Message
 from .forms import RoomForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
@@ -15,7 +15,7 @@ def loginPage(request):
         return redirect('homepage')
 
     if request.method=='POST':
-        uname= request.POST.get('username').lower()
+        uname= request.POST.get('username')
         pwd=request.POST.get('password')
         
         try:
@@ -34,9 +34,11 @@ def loginPage(request):
     context={'page':page}
     return render(request, 'base/login_register.html',context)
 
+
 def logoutUser(request):
     logout(request)
     return redirect('homepage')
+
 
 def registerPage(request):
     form=UserCreationForm()
@@ -55,6 +57,7 @@ def registerPage(request):
     context={'form':form}
     return render(request,'base/login_register.html',context)
 
+
 def home(request):
     query = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms=Room.objects.filter(
@@ -67,10 +70,24 @@ def home(request):
     context={'rooms':rooms, 'topics': topics, 'room_count':room_count}
     return render(request,'base/home.html',context)
 
+
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method=="POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body=request.POST.get('message_sent')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+
+    context={'room':room, 'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -82,6 +99,7 @@ def createRoom(request):
             return redirect('homepage')
     context= {'form':form}
     return render(request,'base/room_form.html',context)
+
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
@@ -100,6 +118,7 @@ def updateRoom(request, pk):
     context={'form':form}
     return render(request, 'base/room_form.html',context)
 
+
 @login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
@@ -111,6 +130,20 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('homepage')
     return render(request,'base/delete.html',{'obj':room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse("You are only allowed to delete your own messages")
+    
+    if request.method == "POST":
+        message.delete()
+        # print(f'room/{message.room.id}')
+        return redirect('room',pk=message.room.id)
+    return render(request,'base/delete.html',{'obj':message})
 
 
 
