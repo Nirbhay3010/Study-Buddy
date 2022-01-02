171875e401832024c46620f92aaca83b4d5d526b
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2022-01-03 00:49:26 +0530

    Wasted 6 hours

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY = config("SECRET_KEY")
+# from decouple import config
+SECRET_KEY = 'django-insecure-1f04w)o*ng&=-k8ocj_d1jzqap+e#u(nte1%d2y#gxr#!@#vwi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
